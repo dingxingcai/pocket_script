809156b2fa0061777ee01e8e9a4fd783f8e38082
dingxc <775862230@qq.com>
2018-03-23 11:41:18 +0800

    测试的command

---
+++ laravel/app/Console/Commands/TestCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Employee;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:TestCommand';
+
+    /**
+     * The console command descphp aription.
+     *
+     * @var string
+     */
+    protected $description = 'TestCommand';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $employee = Employee::select()->where('typeid','00000')->get();
+        echo $employee;
+    }
+}
