45fc4b224e407fc3a62ab52bd50343349671186a
dingxc <775862230@qq.com>
2018-04-13 19:28:15 +0800

    统计各个品牌的销售额占比和音频类商品的销售额占比

--- laravel/app/GraphQL/Query/SaleQuery.php
+++ laravel/app/GraphQL/Query/SaleQuery.php
@@ -50,7 +50,7 @@ class SaleQuery extends Query
         $sales = DB::connection('sqlsrv')->select("select CONVERT(varchar(10), b.billdate, 23) as 'date', sum(r.total) as  'money' 
 from billindex b left join retailBill r on b.BillNumberID = r.BillNumberID inner join ptype p on p.typeId = r.PtypeId 
 where datediff(dd,b.BillDate,getdate()) <= 15  and b.BillType = 305   and p.Parid = '00010'
-and  b.BillDate < CONVERT(varchar(30),getdate(),23)  group by p.ParID,b.BillDate order by date asc;");
+and  b.BillDate <= CONVERT(varchar(30),getdate(),23)  group by p.ParID,b.BillDate order by date asc;");
         foreach ($sales as &$sale) {
 
             //统计当天的总计的销售额
